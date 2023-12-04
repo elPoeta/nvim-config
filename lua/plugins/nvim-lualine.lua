16260f7270cc81833dab7838a588a09043cb0f29
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2023-12-04 08:12:10 -0300

    add gitsigns, refactor lualine conf

--- lua/plugins/nvim-lualine.lua
+++ lua/plugins/nvim-lualine.lua
@@ -3,12 +3,15 @@ local config = function()
 		options = {
 			theme = "auto",
 			globalstatus = true,
-			component_separators = { left = "|", right = "|" },
-			section_separators = { left = "", right = "" },
+      -- component_separators = { left = "|", right = "|" },
+      -- section_separators = { left = "", right = "" },
+      component_separators = { left = '', right = ''},
+      section_separators = { left = '', right = ''},
 		},
 		sections = {
 			lualine_a = { "mode" },
 			lualine_b = { "buffers" },
+      lualine_b = {'branch', 'diff', 'diagnostics', "buffers" },
 			lualine_x = { "encoding", "fileformat", "filetype" },
 			lualine_y = { "progress" },
 			lualine_z = { "location" },
--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -45,6 +45,7 @@ return {
       end,
       integrations = {
         nvimtree = true,
+        gitsigns = true,
         treesitter = true,
         notify = false,
         mini = {
